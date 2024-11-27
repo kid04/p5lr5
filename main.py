5fecbeb07cec2371e2df780dcc06d57bd25455aa
Nicita Sorokolit <kid04@mail.ru>
2024-11-27 18:44:33 +0300

    added decorator for lr6

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+
 import requests
 import time
+import json
 from xml.etree import ElementTree as ET
 class Singleton(type):
     _instances = {}
@@ -8,8 +11,11 @@ class Singleton(type):
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
+class Component():
+    def get_currencies(self) -> str:
+        pass
 
-class CurrenciesLst(metaclass=Singleton):
+class CurrenciesLst(Component, metaclass=Singleton):
     def __init__(self, id_lst, limit=1000):
         self.__id_list = id_lst
         self.__cur_dict = {}
@@ -51,6 +57,7 @@ class CurrenciesLst(metaclass=Singleton):
         pplt.bar(x, y)
         pplt.savefig('currencies.jpg')
 
+
     @property
     def cur_lst(self):
         return self.__cur_dict
@@ -63,11 +70,47 @@ class CurrenciesLst(metaclass=Singleton):
     def id_list(self, lst):
         self.__id_list = lst
 
+class Decorator(Component):
+
+    _component: Component = None
+
+    def __init__(self, component: Component) -> None:
+        self._component = component
+
+    @property
+    def component(self):
+
+        return self._component
+
+    def get_currencies(self) -> str:
+        return self._component.get_currencies()
+
+class ConcreteDecoratorJSON(Decorator):
+    def get_currencies(self) -> str:
+        """
+        Декораторы могут вызывать родительскую реализацию операции, вместо того,
+        чтобы вызвать обёрнутый объект напрямую. Такой подход упрощает
+        расширение классов декораторов.
+        """
+
+        return json.dumps(self.component.get_currencies(), ensure_ascii=False)
+
+class ConcreteDecoratorSCV(Decorator):
+    def get_currencies(self) -> None:
+        currencies = self.component.get_currencies()
+        with open('currencies.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for currency in currencies:
+                row = [currencies[currency][i] for i in range(len(currencies[currency]))]
+                row.insert(0, currency)
+                writer.writerow(row)
+
+
 if __name__ == '__main__':
 
     cclass = CurrenciesLst(['R01090B', 'R01335', 'R01700J'])
+    jclass = ConcreteDecoratorJSON(cclass)
+    CSVclass = ConcreteDecoratorSCV(cclass)
+    print(jclass.get_currencies())
+    print(CSVclass.get_currencies())
 
-    res = cclass.get_currencies()
-    if res:
-        print(cclass.cur_lst)
-        cclass.get_graph()
